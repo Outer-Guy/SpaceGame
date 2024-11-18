a860bf112c100cf7127a85e933c89f687cd9bb5c
Gero <geroxed@gmail.com>
2024-11-18 03:43:39 -0300

    Multiplayer Lobby + Chat

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -9,3 +9,14 @@ enum GAMESTATE {
 var gamestate : GAMESTATE = GAMESTATE.MAIN_MENU
 
 var Players : Dictionary = {}
+var localUsername : String = ""
+
+func load_scene(path : String) -> void :
+	get_tree().root.add_child(load(path).instantiate())
+
+@rpc("any_peer", "call_local")
+func set_gamestate(caller_id : int, state : GAMESTATE) -> void :
+	if(Players.has(caller_id)) :
+		Players[caller_id]["gamestate"] = state
+	if multiplayer.get_unique_id() == caller_id :
+		gamestate = state
