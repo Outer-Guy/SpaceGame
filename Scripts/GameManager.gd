b968019243b343697f13c1fe04e0b15b5e0d5d13
Gero <geroxed@gmail.com>
2024-11-19 20:10:41 -0300

    Multiplayer sync refactor, ready button not working yet

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,42 +1,4 @@
 extends Node
 
-signal onRecievedPlayerData
-
-enum GAMESTATE {
-	MAIN_MENU,
-	LOBBY,
-	PLAYING
-}
-
-var gamestate : GAMESTATE = GAMESTATE.MAIN_MENU
-
-var Players : Dictionary = {}
-var localUsername : String = ""
-
 func load_scene(path : String) -> void :
 	get_tree().change_scene_to_file(path)
-
-@rpc("any_peer", "call_local")
-func set_gamestate(caller_id : int, state : GAMESTATE) -> void :
-	if(Players.has(caller_id)) :
-		Players[caller_id]["gamestate"] = state
-	if multiplayer.get_unique_id() == caller_id :
-		gamestate = state
-
-@rpc("any_peer", "call_remote")
-func send_player_info(id: int, username: String) -> void:
-	if !GameManager.Players.has(id):
-		GameManager.Players[id] = {
-			"id" = id,
-			"username" = username,
-			"gamestate" = GameManager.GAMESTATE.MAIN_MENU,
-			"Ready" = false
-		}
-		
-	onRecievedPlayerData.emit()
-	print("---" + GameManager.localUsername + "---")
-	print(GameManager.Players)
-	print("---------------------")
-	if multiplayer.is_server():
-		for i : int in GameManager.Players:
-			send_player_info.rpc(i, GameManager.Players[i].username)
---
+++ Scripts/PlayerData.gd
@@ -0,0 +1,159 @@
+extends Node
+
+enum GAMESTATE {
+	MAIN_MENU,
+	LOBBY,
+	PLAYING
+}
+
+class PlayerInfo:
+	var id : int
+	var username : String
+	var gamestate : GAMESTATE = GAMESTATE.MAIN_MENU
+	var ready : bool
+	
+	static func create(id : int, username : String) -> PlayerInfo :
+		var player_info: PlayerInfo = PlayerInfo.new()
+		player_info.id = id
+		player_info.username = username
+		player_info.gamestate = GAMESTATE.MAIN_MENU
+		player_info.ready = false
+		return player_info
+	
+	static func serialize(data : PlayerInfo) -> Array :
+		return [data.id, data.username, data.gamestate, data.ready]
+	
+	static func deserialize(serializedData : Array) -> PlayerInfo :
+		var data : PlayerInfo = PlayerInfo.create(serializedData[0], serializedData[1])
+		data.gamestate = serializedData[2]
+		data.ready = serializedData[3]
+		return data
+	
+	func log() -> void :
+		var msg : String = ""
+		msg += "\n("
+		if self.id == 1 : msg += "Host"
+		else : msg += "Client"
+		msg += ", ID: " + str(self.id) + ") " + self.username + ": "
+		msg += "is "
+		match self.gamestate:
+			GAMESTATE.MAIN_MENU:
+				msg += "on Main Menu"
+			GAMESTATE.LOBBY:
+				msg += "on Lobby, "
+				if !self.ready: msg += "not "
+				msg += "ready"
+			GAMESTATE.PLAYING:
+				msg += "playing"
+		# EXAMPLE : 
+		# (Host, ID: 1) Gero: is on Lobby, not ready
+		print(msg)
+
+func _ready() -> void:
+	multiplayer.peer_disconnected.connect(on_peer_disconnected)
+
+# Stores all PlayerInfo from players, should not be accessed directly outside this script
+var all_data : Dictionary = {}
+signal on_all_data_set
+
+# -------------- GETTERS --------------------
+# Returns PlayerInfo referring to local player
+var local : PlayerInfo:
+	get:
+		return all_data[multiplayer.get_unique_id()]
+
+# Returns PlayerInfo from id
+func by_id(id : int) -> PlayerInfo : return all_data[id]
+
+# Return PlayerInfo searching all players with the username
+func by_username(user : String) -> Array :
+	var list : Array = []
+	for info : PlayerInfo in all_data.values() : 
+		if info.username == user : list.append(info)
+	return list
+
+func all_ready() -> bool :
+	var ready : bool = true
+	for data : PlayerInfo in all_data :
+		ready = ready && data.ready
+	return ready
+
+# -------------- SETTERS --------------------
+func check_id(id : int) -> bool :
+	if(all_data.has(id)) :
+		return true
+	else : 
+		printerr(local.username + " tried to set info of inexistent player, id: "
+		+ str(id) + ". Asking for sync.")
+		
+		ask_for_missing.rpc_id(1)
+		return false
+
+@rpc("any_peer", "call_local")
+func set_username(id : int, username : String) -> void :
+	if check_id(id) : all_data[id].username = username
+
+
+@rpc("any_peer", "call_local")
+func set_ready(id : int, state : bool) -> void :
+	if check_id(id) : all_data[id].ready = state
+	if multiplayer.is_server() && all_ready() :
+		pass # TODO
+
+@rpc("any_peer", "call_local")
+func set_gamestate(id: int , state : GAMESTATE) -> void :
+	if check_id(id) : all_data[id].gamestate = state
+
+# -------------- SYNCING --------------------
+
+func on_peer_disconnected(id : int) -> void :
+	if multiplayer.is_server() :
+		remove_player_data.rpc(id)
+
+@rpc("any_peer", "call_remote")
+func ask_for_missing() -> void :
+	if multiplayer.is_server() :
+		for data : PlayerInfo in all_data.values():
+			create_player_data.rpc_id(multiplayer.get_remote_sender_id(), data.id, data.username)
+	else : printerr("Some client is trying to send missing info.")
+
+@rpc("any_peer", "call_remote")
+func sync_all() -> void :
+	if multiplayer.is_server() :
+		var serialized_all_data : Array = []
+		for data : PlayerInfo in all_data.values():
+			serialized_all_data.append(PlayerInfo.serialize(data))
+		set_all_data.rpc(serialized_all_data)
+
+# Create PlayerInfo if non existent
+@rpc("authority", "call_local")
+func create_player_data(id : int, username : String) -> void :
+	if !all_data.has(id):
+		all_data[id] = PlayerInfo.create(id, username)
+
+# Remove PlayerInfo if existent
+@rpc("authority", "call_local")
+func remove_player_data(id : int) -> void :
+	if all_data.has(id):
+		all_data.erase(id)
+
+@rpc("any_peer", "call_remote")
+func on_player_connected(id: int, username: String) -> void:
+	if multiplayer.is_server() :
+		create_player_data(id, username)
+		sync_all()
+		
+
+# Override all_data sent
+@rpc("authority", "call_remote")
+func set_all_data(serialized_all_data : Array) -> void :
+	for serialized_data : Array in serialized_all_data :
+		set_player_data(serialized_data)
+	on_all_data_set.emit()
+
+# Override PlayerData sent
+@rpc("authority", "call_remote")
+func set_player_data(serialized_data : Array) -> void :
+	# serialized_data[0] has a reference to the id sent
+	if all_data.has(serialized_data[0]) : all_data.erase(serialized_data[0])
+	all_data[serialized_data[0]] = PlayerInfo.deserialize(serialized_data)
